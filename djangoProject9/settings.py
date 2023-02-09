f7adc43476cc9386c33079ee1bac59086d14ecb1
Uppicki <81292510+Uppicki@users.noreply.github.com>
2023-02-09 15:23:25 +0300

    frontend was created

--- djangoProject9/settings.py
+++ djangoProject9/settings.py
@@ -176,3 +176,6 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 VIDEOS_DIR = os.path.join(BASE_DIR, 'videos')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- djangoProject9/urls.py
+++ djangoProject9/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 from djangoProject9 import settings
@@ -30,3 +31,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
